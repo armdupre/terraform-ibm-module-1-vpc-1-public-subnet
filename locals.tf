f81b15f6d9ad784b2a772b6360255de457e76d28
armdupre <a-joseph.dupre@keysight.com>
2023-06-05 13:59:41 -0400

    Updated.

--- locals.tf
+++ locals.tf
@@ -1,15 +1,16 @@
 locals {
 	InboundIPv4CidrBlock = var.InboundIPv4CidrBlock
-	PublicSecurityGroupName = "${local.UserLoginTag}-${local.Tag}-${local.Version}-public-security-group"
+	Preamble = "${local.UserLoginTag}-${local.Tag}-${local.Version}"
+	PublicSecurityGroupName = "${local.Preamble}-public-security-group"
 	PublicSubnetCidrBlock = "10.0.10.0/24"
-	PublicSubnetName = "${local.UserLoginTag}-${local.Tag}-${local.Version}-public-subnet"
+	PublicSubnetName = "${local.Preamble}-public-subnet"
 	PublicSubnetZone = var.PublicSubnetZone
 	Tag = var.Tag
 	UserEmailTag = var.UserEmailTag
 	UserLoginTag = var.UserLoginTag
 	UserProjectTag = var.UserProjectTag
 	Version = var.Version
-	VpcAddressPrefixName = "${local.UserLoginTag}-${local.Tag}-${local.Version}-vpc-address-prefix"
+	VpcAddressPrefixName = "${local.Preamble}-vpc-address-prefix"
 	VpcCidrBlock = "10.0.0.0/16"
-	VpcName = "${local.UserLoginTag}-${local.Tag}-${local.Version}-vpc"
+	VpcName = "${local.Preamble}-vpc"
 }
